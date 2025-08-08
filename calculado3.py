322b5703d7f1d3ffc282a8b2ef811fcb1ba1767e
Mauricio <mauriciolmlchavarria@gmail.com>
2025-08-07 20:20:18 -0600

    SUBO TAREA CALCULADORA SIMPLE

---
+++ calculado3.py
@@ -0,0 +1,6 @@
+# Programa básico: sumar dos números e imprimir el resultado (IMPORTANTE: use chatgpt pero si aprendi algo amy tkm)
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+
+resultado = num1 + num2
+print("La suma es:", resultado)
